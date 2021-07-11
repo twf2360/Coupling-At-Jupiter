f3243e6b2932705ba527080e0d98b4d4664e481a
Tom Fell <tomfell2360@gmail.com>
2021-07-11 17:55:55 +0100

    quick look over things before I remembered that doing the reading is probably much more important

--- pre-project/particle-E-B-FIELD.py
+++ pre-project/particle-E-B-FIELD.py
@@ -1,13 +1,21 @@
 '''
 Just defining a particle, to then define it's motion in E fields, B fields, and then both? 
 '''
-
+import numpy as np
 class particle:
     
     def __init__(self, mass, charge, r, theta, phi, velocity, acceleration):
+        #theta = colatitude, phi = azimuthal
         '''
         defining the particle, position is defined using spherical co-ordinates - can always change to xy at a later date.
         '''
+        self.mass = mass
+        self.charge = charge 
+        self.radial = r
+        self.azimuthal = phi
+        self.colatitude = theta
+        self.velocity = velocity
+        self.acceleration = acceleration
 
     def kinetic_energy(self):
         '''
@@ -18,16 +26,18 @@ class particle:
         ke = 0.5*self.mass*np.linalg.norm(self.velocity**2)
         return ke 
 
+
 class field_line:
     '''
-    initial precursor to the field class - how does a particle move on a single field line? 
+    initial precursor to the field class - how does a particle move on a single field line?
+    it might be easier to model this using xy co-ordinates for the particle instead of using spherica; 
     '''
-class field:
+class dipole_field:
     '''
     define the field that the particle is moving through
     '''
+    
 
-    ''' need to think about how we're doing this - field lines? defining start and end? '''
         
 
 
